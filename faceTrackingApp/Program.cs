225ca3bb3e536dc2fbbde23f74c9489b14c964dd
Cigdem TURAN <13900943r@connect.polyu.hk>
2017-10-12 23:17:06 +0800

    Creating the program

--- faceTrackingApp/Program.cs
+++ faceTrackingApp/Program.cs
@@ -3,13 +3,113 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Face;
+using System.IO;
 
 namespace faceTrackingApp
 {
     class Program
     {
+        public static KinectSensor _sensor = null;
+        public static BodyFrameReader _bodyFrameReader = null;
+        public static HighDefinitionFaceFrameSource _faceSource = null;
+        public static HighDefinitionFaceFrameReader _faceReader = null;
+        public static FaceAlignment _faceAlignment = null;
+        public static FaceModel _faceModel = null;
+        public static CoordinateMapper _coordinateMapper = null;
+        public static Worker _worker = new Worker();
+        public static FileStream _animationUnitStream, _verticesFileStream;
+        public static bool trackingSuccess = false;
+
         static void Main(string[] args)
         {
+            _sensor = KinectSensor.GetDefault();
+
+            _worker.getSubjectID();
+
+            if (_sensor != null)
+            {
+                _sensor.Open();
+                Console.WriteLine("sensorOpened");
+                if (_sensor.IsOpen)
+                {
+                    _coordinateMapper = _sensor.CoordinateMapper;
+                    _bodyFrameReader = _sensor.BodyFrameSource.OpenReader();
+
+                    _bodyFrameReader.FrameArrived += BodyFrameReader_FrameArrived;
+
+                    _faceSource = new HighDefinitionFaceFrameSource(_sensor);
+                    _faceReader = _faceSource.OpenReader();
+                    _faceReader.FrameArrived += FaceReader_FrameArrived;
+
+                    _faceModel = new FaceModel();
+                    _faceAlignment = new FaceAlignment();
+
+                }
+            }
+            string input = Console.ReadLine();
+            _sensor.Close();
+        }
+
+        private static void BodyFrameReader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            BodyFrame _bodyFrame = e.FrameReference.AcquireFrame();
+            if (_bodyFrame == null)
+            {
+                return;
+            }
+
+            if (_worker.counterFile == 0 && _worker.counterFrame == 0)
+            {
+                _worker.CreateFolders();
+                _worker.InilizeAnimationUnitStream();
+            }
+
+            _bodyFrame.GetAndRefreshBodyData(_worker._bodies);
+
+            Body body = _worker._bodies.Where(b => b.IsTracked).FirstOrDefault();
+
+            if (!_faceSource.IsTrackingIdValid)
+            {
+                if (body != null)
+                {
+                    _faceSource.TrackingId = body.TrackingId;
+                }
+            }
+
+        }
+
+        static void FaceReader_FrameArrived(object sender, HighDefinitionFaceFrameArrivedEventArgs e)
+        {
+            TimeSpan milliseconds = DateTime.Now.TimeOfDay;
+            using (var _faceFrame = e.FrameReference.AcquireFrame())
+            {
+
+                if (_faceFrame != null && _faceFrame.IsFaceTracked)
+                {
+                    if (!trackingSuccess)
+                    {
+                        Console.WriteLine("started face tracking..");
+                        trackingSuccess = true;
+                    }
+                    _faceFrame.GetAndRefreshFaceAlignmentResult(_faceAlignment);
+                    Task.Factory.StartNew(() =>
+                    {
+                        _worker.WriteAnimationUnits(_faceAlignment, milliseconds);
+                    });
+                }
+                else
+                {
+                    if (trackingSuccess)
+                    {
+                        Console.WriteLine("tracking is lost..");
+                        trackingSuccess = false;
+                    }
+                    _worker.WriteAnimationUnits(_faceAlignment, milliseconds, false);
+                }
+            }
+
         }
     }
 }
---
+++ faceTrackingApp/Worker.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Face;
+using System.Xml;
+using System.IO;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows;
+using System.Drawing;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows;
+using System.Runtime.InteropServices;
+
+namespace faceTrackingApp
+{
+    class Worker
+    {
+        public string _fileName { get; set; }
+        private string _folderNameRec = null;
+        private string _animationUnitFolder;
+        public int counterFile;
+        public int counterFrame;
+        public Body[] _bodies = new Body[6];
+        private StreamWriter _animationUnitWriter = null;
+
+        public void getSubjectID()
+        {
+            Console.WriteLine("enter the subject ID");
+            string input = Console.ReadLine();
+            _folderNameRec = Path.Combine(@"C:\NOISE_PATTERN\SAVINGS\", input);
+            if (!Directory.Exists(_folderNameRec))
+            {
+                Directory.CreateDirectory(_folderNameRec);
+                Console.WriteLine("folder is created..");
+            }
+        }
+
+        public void CreateFolders()
+        {
+            _animationUnitFolder = Path.Combine(_folderNameRec, "AnU");
+            Directory.CreateDirectory(_animationUnitFolder);
+        }
+        public void InilizeAnimationUnitStream()
+        {
+            string animationUnitFile = _animationUnitFolder + "AnU_" + counterFile + ".txt";
+            FileStream animationUnitStream = new FileStream(animationUnitFile, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            _animationUnitWriter = new StreamWriter(animationUnitStream);
+        }
+
+        public void WriteAnimationUnits(FaceAlignment _faceAlignment, TimeSpan milliseconds, bool flag = true)
+        {
+            _animationUnitWriter.Write("\r\n");
+            _animationUnitWriter.Write("{0}, ", milliseconds);
+            if (flag)
+            {
+                IReadOnlyDictionary<FaceShapeAnimations, float> _animationUnits = _faceAlignment.AnimationUnits;
+                Dictionary<FaceShapeAnimations, float> _animationUnitsDict = new Dictionary<FaceShapeAnimations, float>();
+
+                foreach (FaceShapeAnimations _anuType in _animationUnits.Keys)
+                {
+                    _animationUnitWriter.Write("{0}, ", _animationUnits[_anuType]);
+                }
+            }
+            else
+            {
+                _animationUnitWriter.Write("NaN, ");
+            }
+            _animationUnitWriter.Flush();
+        }
+
+    }
+}
